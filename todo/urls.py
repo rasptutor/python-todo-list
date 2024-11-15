1336ad9f7677d90d41ae6edc2216eee1d2e3f0dc
nelio <njafonso@gmail.com>
2024-11-15 20:30:45 +0200

    first commit

---
+++ todo/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import TaskDelete, TaskCreate, TaskDetail, TaskUpdate, home, TaskList
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('tasks/', TaskList.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('task/create/', TaskCreate.as_view(),name='task-create'),
+    path('task/update/<int:pk>/', TaskUpdate.as_view(),name='task-update'),
+    path('task/delete/<int:pk>/', TaskDelete.as_view(),name='task-delete'),
+]
+
